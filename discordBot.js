319885c203a4b678495cac4251ded85d52685daa
yaman1337 <yaman.sarabariya@gmail.com>
2022-10-15 22:07:55 +0545

    commonjs to es6 module + prettify code

--- discordBot.js
+++ discordBot.js
@@ -1,34 +1,40 @@
 //initializing dotenv file
-require('dotenv').config();
+import dotenv from "dotenv";
+dotenv.config();
 
 const config = process.env;
 
 //connecting discord.js
-const { Client, GatewayIntentBits, Partials } = require('discord.js');
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+
 const client = new Client({
-    intents: [
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildBans,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ],
-    partials: [Partials.Channel]
+  intents: [
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [Partials.Channel],
 });
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`)
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('messageCreate', msg => {
-    try {
-        if (msg.content === "Hello" || msg.content === "Hi" || msg.content === "Hey") {
-            msg.reply(`Hey there ${msg.author.username}`)
-        }
-    } catch (error) {
-        console.log(error)
+client.on("messageCreate", (msg) => {
+  try {
+    if (
+      msg.content === "Hello" ||
+      msg.content === "Hi" ||
+      msg.content === "Hey"
+    ) {
+      msg.reply(`Hey there ${msg.author.username}`);
     }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //Log in our bot
-client.login(config.CLIENT_TOKEN)
+client.login(config.CLIENT_TOKEN);
